e50763a4d55d95ff79e08a4d856cc295d31e360f
Marina Kholodova <marinaholodova@gmail.com>
2025-05-01 13:26:38 +0300

    feat: configure Prettier RSS-ECOMM-1_11.

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,45 @@
-﻿import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginVue from 'eslint-plugin-vue';
-import globals from 'globals';
-import typescriptEslint from 'typescript-eslint';
-
+﻿import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import typescriptEslint from "typescript-eslint";
 
 export default typescriptEslint.config(
-  { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
+  { ignores: ["*.d.ts", "**/coverage", "**/dist"] },
   {
     extends: [
       eslint.configs.recommended,
       ...typescriptEslint.configs.recommended,
-      ...eslintPluginVue.configs['flat/recommended'],
+      ...eslintPluginVue.configs["flat/recommended"],
+      eslintConfigPrettier,
     ],
-    files: ['**/*.{ts,vue}'],
+    files: ["**/*.{ts,vue}"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: globals.browser,
       parserOptions: {
         parser: typescriptEslint.parser,
       },
     },
-
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     rules: {
-      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/explicit-member-accessibility': [
-        'error',
-        { accessibility: 'explicit', overrides: { constructors: 'off' } },
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
       ],
-      '@typescript-eslint/member-ordering': 'error',
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { accessibility: "explicit", overrides: { constructors: "off" } },
+      ],
+      "@typescript-eslint/member-ordering": "error",
+
+      "prettier/prettier": "error",
     },
   },
   eslintConfigPrettier,
