4f811877ad124bc4fe87cb5b949ab731bc7d4858
ZhannaIvashova <zhannaklepikova90@gmail.com>
2025-05-09 15:09:10 +0400

    feat: add button base, add login form

---
+++ src/components/ui/TheButton.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps<{
+  text: string;
+  size?: "sm" | "md" | "lg" | "xl";
+  type?: "button" | "submit";
+  variant?: "primary" | "disabled";
+}>();
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+</script>
+
+<template>
+  <button
+    :class="[
+      'base-button',
+      `type-${type}`,
+      `size-${size}`,
+      `variant-${variant}`,
+    ]"
+    @click="emit('click')"
+  >
+    {{ props.text }}
+  </button>
+</template>
+
+<style scoped>
+.base-button {
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  width: 100%;
+  font-weight: 500;
+}
+
+.size-xl {
+  max-width: 267px;
+  height: 49px;
+  font-size: 16px;
+}
+
+.size-lg {
+  max-width: 199px;
+  height: 49px;
+  font-size: 16px;
+}
+
+.size-md {
+  max-width: 200px;
+  height: 44px;
+  font-size: 14px;
+}
+
+.size-sm {
+  max-width: 164px;
+  height: 44px;
+  font-size: 14px;
+}
+
+.variant-primary {
+  background-color: var(--blue);
+  color: var(--white);
+}
+
+.variant-disabled {
+  pointer-events: none;
+  background-color: var(--blue-light);
+  color: var(--grey-not-active);
+}
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,7 +1,83 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import TheButton from "@/components/ui/TheButton.vue";
+
+const email = ref("");
+const password = ref("");
+
+const isFromValid = computed(() => {
+  return email.value.trim().length > 0 && password.value.trim().length > 0;
+});
+
+const handleForm = (): void => console.log("форма отправлена");
+</script>
 
 <template>
-  <h1>Login Page</h1>
+  <div class="container">
+    <h1>Войти</h1>
+    <div class="register-prompt">
+      <span>Ещё нет аккаунта?</span>
+      <router-link to="/register">Зарегистрироваться</router-link>
+    </div>
+    <form @submit.prevent="handleForm">
+      <div class="form">
+        <div class="form-email">
+          <label for="email">Почта</label>
+          <input id="email" v-model="email" type="email" />
+        </div>
+        <div class="form-password">
+          <label for="password">Пароль</label>
+          <input id="password" v-model="password" type="password" />
+        </div>
+      </div>
+      <TheButton
+        text="Войти"
+        type="submit"
+        size="xl"
+        :variant="isFromValid ? 'primary' : 'disabled'"
+      />
+    </form>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 542px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 96px 40px 40px;
+  border-radius: 40px;
+  background-color: var(--blue-lighter);
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 28px;
+  text-align: center;
+}
+
+.register-prompt {
+  display: flex;
+  gap: 14px;
+  align-items: baseline;
+  justify-content: center;
+}
+
+.register-prompt a {
+  color: var(--black);
+  text-decoration: none;
+  border-bottom: 1px solid var(--black);
+  line-height: 1.5;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  margin: 40px 0;
+}
+</style>
