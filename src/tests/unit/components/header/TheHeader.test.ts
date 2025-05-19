b44762747d1a6144b2d737ee51a9a204ee386d4d
Tatiana <bodtata@yandex.ru>
2025-05-19 13:43:18 +0200

    fix: add setupr and lang ts in baseContainer

---
+++ src/tests/unit/components/header/TheHeader.test.ts
@@ -0,0 +1,11 @@
+// import { describe, it, vi } from "node:test";
+// import { mount } from "@vue/test-utils";
+// import { useRoute } from "vue-router";
+// import { useRouter } from "vue-router";
+// import { useTokenStore } from "@/store/useTokenStore";
+
+// vi.mock
+
+// describe('<TheHeader />', () => {
+
+// })
