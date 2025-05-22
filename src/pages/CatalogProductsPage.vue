b4a834fd0420dbe24b9c4ae3228f4376a38b9ba4
Tatiana <bodtata@yandex.ru>
2025-05-22 18:02:20 +0200

    feat: add a product card

--- src/pages/CatalogProductsPage.vue
+++ src/pages/CatalogProductsPage.vue
@@ -1,7 +1,18 @@
-<script lang="ts"></script>
+<script lang="ts" setup>
+import ProductCard from "@/components/products/ProductCard.vue";
+</script>
 
 <template>
-  <h1>Products page</h1>
+  <div>
+    <h1>Products page</h1>
+    <ProductCard
+      title="Samsung"
+      price="12345"
+      image=""
+      discountedprice="1234"
+      description="sdfsdfsdfddfgdfgdfgdfgdf"
+    />
+  </div>
 </template>
 
 <style scoped></style>
--- src/pages/TabletsPage.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts"></script>
-
-<template>
-  <h1>tabltes page</h1>
-</template>
-
-<style scoped></style>
