898741d4a893a46a72de3702b723ae506beb10a5
Tatiana <bodtata@yandex.ru>
2025-05-28 15:47:51 +0200

    feat: move plugin import to index.ts with pinia

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,10 @@ import App from "./App.vue";
 import router from "./router";
 import pinia from "./store";
 import { useAuthStore } from "./store/useAuthStore";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 const app = createApp(App);
 app.use(pinia);
 app.use(router);
-pinia.use(piniaPluginPersistedstate);
 
 app.mount("#app");
 
