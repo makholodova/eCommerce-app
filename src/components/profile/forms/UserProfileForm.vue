ffd07faf67d22a9a9cc15261a34972593823f290
Marina Kholodova <marinaholodova@gmail.com>
2025-06-01 16:16:39 +0300

    feat: disable profileInfo form during submission

--- src/components/profile/forms/UserProfileForm.vue
+++ src/components/profile/forms/UserProfileForm.vue
@@ -8,6 +8,10 @@ import BaseButton from "@/components/ui/BaseButton.vue";
 
 const modelValue = defineModel<UserFormModel>({ required: true });
 
+const { isLoading = false } = defineProps<{
+  isLoading?: boolean;
+}>();
+
 const emit = defineEmits<{
   (e: "close"): void;
   (e: "submit"): void;
@@ -34,60 +38,63 @@ const isFormValid = computed(() => !v$.value.$invalid);
 
 <template>
   <div>
-    <form class="form-content" @submit.prevent="handleSubmit">
-      <BaseInputField
-        id="firstName"
-        v-model="modelValue.firstName"
-        name="firstName"
-        :vuelidate-rules="rules.firstName"
-        label="Имя"
-        placeholder="Иван"
-        show-error
-        type="text"
-        autocomplete="given-name"
-      />
+    <form @submit.prevent="handleSubmit">
+      <fieldset :disabled="isLoading" class="form-content form-fieldset">
+        <BaseInputField
+          id="firstName"
+          v-model="modelValue.firstName"
+          name="firstName"
+          :vuelidate-rules="rules.firstName"
+          label="Имя"
+          placeholder="Иван"
+          show-error
+          type="text"
+          autocomplete="given-name"
+        />
 
-      <BaseInputField
-        id="lastName"
-        v-model="modelValue.lastName"
-        name="lastName"
-        :vuelidate-rules="rules.lastName"
-        label="Фамилия"
-        placeholder="Иванов"
-        show-error
-        type="text"
-        autocomplete="family-name"
-      />
+        <BaseInputField
+          id="lastName"
+          v-model="modelValue.lastName"
+          name="lastName"
+          :vuelidate-rules="rules.lastName"
+          label="Фамилия"
+          placeholder="Иванов"
+          show-error
+          type="text"
+          autocomplete="family-name"
+        />
 
-      <BaseInputField
-        id="birthDate"
-        v-model="modelValue.dateOfBirth"
-        name="birthDate"
-        :vuelidate-rules="rules.dateOfBirth"
-        label="Дата рождения"
-        placeholder="01.01.2000"
-        show-error
-        type="date"
-        autocomplete="date-of-birth"
-      />
+        <BaseInputField
+          id="birthDate"
+          v-model="modelValue.dateOfBirth"
+          name="birthDate"
+          :vuelidate-rules="rules.dateOfBirth"
+          label="Дата рождения"
+          placeholder="01.01.2000"
+          show-error
+          type="date"
+          autocomplete="date-of-birth"
+        />
 
-      <BaseInputField
-        id="email"
-        v-model="modelValue.email"
-        name="email"
-        :vuelidate-rules="rules.email"
-        label="E-mail"
-        placeholder="user@example.com"
-        show-error
-        type="email"
-        autocomplete="email"
-      />
+        <BaseInputField
+          id="email"
+          v-model="modelValue.email"
+          name="email"
+          :vuelidate-rules="rules.email"
+          label="E-mail"
+          placeholder="user@example.com"
+          show-error
+          type="email"
+          autocomplete="email"
+        />
+      </fieldset>
       <div class="modal-actions">
         <BaseButton
           size="xl"
           text="Сохранить"
           type="submit"
-          :disabled="!isFormValid"
+          :is-loading="isLoading"
+          :disabled="!isFormValid || isLoading"
         />
         <BaseButton
           size="xl"
@@ -115,7 +122,11 @@ const isFormValid = computed(() => !v$.value.$invalid);
   justify-content: flex-end;
   gap: 0.5rem;
 }
-
+.form-fieldset {
+  border: none;
+  padding: 0;
+  margin: 0;
+}
 @media (max-width: 650px) {
   .form-content {
     grid-template-columns: repeat(1, 1fr);
